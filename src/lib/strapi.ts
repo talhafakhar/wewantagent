70749535df0ff14db6320f57a34f5fee14194bb4
billalkhanzada <bilalll9889@gmail.com>
2025-10-22 19:46:47 +0500

    token update

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -1,6 +1,6 @@
 import type { BlogPost, BlogResponse } from "@/types/blog";
 
-const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL || "http://localhost:1337";
+const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL || "https://api.wewantagent.com";
 const API_TOKEN = process.env.STRAPI_API_TOKEN;
 
 if (!API_TOKEN) {
