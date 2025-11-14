54022e28ef67ea5d8133dc5a9eee0166602351f3
billalkhanzada <bilalll9889@gmail.com>
2025-11-14 17:28:11 +0500

    fix svg issue

--- src/components/HomePage/FlowSection.tsx
+++ src/components/HomePage/FlowSection.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, {useRef, useState} from "react";
+import React, {useRef} from "react";
 import {motion, useScroll, useTransform} from "framer-motion";
 import Image from "next/image";
 import {
